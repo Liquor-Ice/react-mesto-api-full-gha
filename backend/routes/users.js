02725f7586bea9638bf0f5d91135be3fc0e08221
Liquor-Ice <doppelgangerer@yandex.ru>
2024-02-07 13:50:01 +0300

    исправляет роут аватара

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -23,7 +23,7 @@ router.patch('/me', celebrate({
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.required().string().pattern(/(https?:\/\/)(w{3}\.)?([\w-]+)\.(\S+)(#$)?/),
+    avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?([\w-]+)\.(\S+)(#$)?/).required(),
   }),
 }), updateAvatar);
 
